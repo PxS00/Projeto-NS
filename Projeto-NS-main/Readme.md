ec6ab9842ffb172cbd821e978e7e984de8dec00d
Lucas Rossoni <lucas.rossonidd@gmail.com>
2025-03-05 13:21:47 -0300

    first commit

---
+++ Projeto-NS-main/Readme.md
@@ -0,0 +1,29 @@
+# 📌 Meu Primeiro Projeto - Numero-Secreto
+
+Este é um projeto de teste para aprender a usar o Git e o GitHub e estudos de prática com JavaScript e interações no HTML.
+
+## ⚙️ Tecnologias Utilizadas
+![HTML](https://img.shields.io/badge/HTML-3CB371?style=for-the-badge&logo=html5&logoColor=white) ![CSS](https://img.shields.io/badge/CSS-3CB371?style=for-the-badge&logo=css3&logoColor=white) ![JavaScript](https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black)
+
+## Como Usar
+
+1. Clone este repositório:
+   ```sh
+   git clone https://github.com/PxS00/Numero-Secreto-v2.git
+   ```
+2. Acesse a pasta do projeto:
+   ```sh
+   cd Numero-Secreto-v2
+   ```
+3. Execute o projeto conforme necessário.
+
+## 📝 Sobre
+
+Criado para fins de aprendizado e prática com Git e GitHub e estudo com JavaScript e HTML.
+
+## Time
+
+<img src="https://github.com/user-attachments/assets/2ef65fde-7f34-441f-95f9-089d429e73fb" width="150px">
+
+
+ **Feito por [Lucas Rossoni Dieder](https://github.com/PxS00)** 
---
+++ Projeto-NS-main/app.js
@@ -0,0 +1,59 @@
+let listaDeNumerosSorteados = [];
+let numeroLimite = 100;
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+
+const exibirTextoNaTela = (tag, texto) => document.querySelector(tag).innerHTML = texto; //Arrow functions
+const input = document.querySelector('input');
+const reiniciarBtn = document.getElementById ('reiniciar');
+
+const exibirMensagemInicial = () => {
+    exibirTextoNaTela('h1', 'Jogo do Número Secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
+}
+exibirMensagemInicial()
+
+//Função + nome da ação no Html
+function verificarChute() {
+    let chute = Number(input.value); //Garantindo que chute seja tratado como número
+
+    if (isNaN(chute) || chute < 1 || chute > numeroLimite) { //se chute não for um número, for abaixo de 1 e acima do número limite, o texto da como erro
+        exibirTextoNaTela('h1, Erro!');
+        exibirTextoNaTela('p', 'Por favor insira um número entre 1 e 100');
+        return;
+    }
+
+    if (chute === numeroSecreto) {
+        exibirTextoNaTela('h1', 'Parabéns você acertou!');
+        exibirTextoNaTela('p', `Você descobriu o número secreto com ${tentativas} ${tentativas > 1 ? 'tentativas' : 'tentativa'}!`) //operador ternário
+        reiniciarBtn.removeAttribute('disabled');
+    } else {
+        exibirTextoNaTela('h1', 'Errou :(');
+        exibirTextoNaTela('p', `O número secreto é ${chute > numeroSecreto ? 'menor' : 'maior'}`); //operador ternário
+    }
+    tentativas++
+    limparCampo()
+    console.log(numeroSecreto);
+}
+
+function gerarNumeroAleatorio() {
+    if (listaDeNumerosSorteados.length === numeroLimite) listaDeNumerosSorteados = []; //Se a lista for igual NS, reinicia a lista
+    let numeroEscolhido;
+    //Usando do e while para gerar número aleatório sem recursão
+    do {
+        numeroEscolhido = Math.floor(Math.random() * numeroLimite + 1); //Gere um número aleatorio sendo ele inteiro 
+    } while (listaDeNumerosSorteados.includes(numeroEscolhido)); //procura se o número sorteado esta na lista
+
+    listaDeNumerosSorteados.push(numeroEscolhido); //caso não, ele adiciona a lista
+    console.log(listaDeNumerosSorteados);
+    return numeroEscolhido; // e volta para a variavel
+}
+const limparCampo = () => document.querySelector('input').value = ''; //Arrow functions
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    reiniciarBtn.setAttribute('disabled', true);
+}
